404f25911d80bfbb8944f9fdb4de3e6866f1e1f8
david kim <dkreliable@gmail.com>
2025-02-02 10:57:12 -0500

    fix

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log( "1234")
+
+let kim = false;
+
+
+function toggleContrast() {
+       kim = !kim;
+    if(kim)
+       {document.body.classList += " js-dark-theme"}
+    else {
+        document.body.classList.remove("js-dark-theme")
+    }
+
+
+
+}
+
+
