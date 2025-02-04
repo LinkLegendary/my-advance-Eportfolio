b0bb6d54f40e6babc16717502557cd0eab19eb1f
david kim <dkreliable@gmail.com>
2025-02-04 18:36:18 -0500

    fix

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
+// template id :   template_rlm7kvs
+// service id : service_6vzscix
+// public key : NIiu40ksu_AbcP-Mb
+
+/*
+<script type="text/javascript">
+   (function(){
+      emailjs.init({
+        publicKey: "NIiu40ksu_AbcP-Mb",
+      });
+   })();
+</script>
+
+*/
+
+
+
+
+
 let kim = false;
 
 
@@ -15,3 +34,67 @@ function toggleContrast() {
 }
 
 
+
+
+
+
+
+
+
+
+
+function contact(event){
+    event.preventDefault();
+
+    const loading  =  document.querySelector('.modal-overlay-loading')
+    const success  =  document.querySelector('.modal-overlay-success')
+    loading.classList += ' js-modal-overlay-visible'
+
+
+
+
+    emailjs
+    .sendForm(
+       'service_6vzscix',
+       'template_rlm7kvs',
+       event.target,
+
+    ).then(() => {
+      loading.classList.remove('js-modal-overlay-visible');
+      success.classList += ' js-modal-overlay-visible';
+   
+    }).catch(() => {
+      loading.classList.remove('js-modal-overlay-visible');
+      alert(
+         "The email service is temperaily unavailable. please contact me kkk@gmail.com"
+      )
+    })
+
+
+
+   
+    setTimeout(() => { 
+      loading.classList.remove('js-modal-overlay-visible');
+      success.classList += ' js-modal-overlay-visible';
+      
+      
+    },2000);
+
+}
+
+
+
+
+
+
+
+let isModalOpen = false;
+function toggleModal() {
+   if (isModalOpen) {
+     isModalOpen = false;
+     return document.body.classList.remove("js-modal-open");
+   }
+   isModalOpen = true;
+   document.body.classList += " js-modal-open";
+ }
+   
